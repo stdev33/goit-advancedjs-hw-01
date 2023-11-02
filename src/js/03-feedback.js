1d83b0cd85eac27f776bfbf5b753d63b43b2075f
Serhii Tarasenko <stdev33@gmail.com>
2023-11-02 17:48:01 +0200

    [+] task 3. save and restore feedback form fields

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+import throttle from 'lodash.throttle';
 
+const LS_FEEDBACK_FORM_STATE_KEY = 'feedback-form-state';
+const saveStateInterval = 500;
+
+const feedbackForm = document.querySelector('.feedback-form');
+
+const feedbackFormState = loadFeedbackFormState();
+
+feedbackForm.addEventListener(
+  'input',
+  throttle(onFeedbackFormInput, saveStateInterval)
+);
+feedbackForm.addEventListener('submit', onFeedbackFormSubmit);
+
+function loadFeedbackFormState() {
+  const feedbackFormState = localStorage.getItem(LS_FEEDBACK_FORM_STATE_KEY);
+
+  if (feedbackFormState) {
+    const parsedFeedbackFormState = JSON.parse(feedbackFormState);
+
+    feedbackForm.email.value = parsedFeedbackFormState.email;
+    feedbackForm.message.value = parsedFeedbackFormState.message;
+    return parsedFeedbackFormState;
+  }
+
+  return {
+    email: '',
+    message: '',
+  };
+}
+
+function onFeedbackFormInput() {
+  feedbackFormState.email = feedbackForm.email.value;
+  feedbackFormState.message = feedbackForm.message.value;
+
+  localStorage.setItem(
+    LS_FEEDBACK_FORM_STATE_KEY,
+    JSON.stringify(feedbackFormState)
+  );
+}
+
+function onFeedbackFormSubmit(event) {
+  event.preventDefault();
+
+  feedbackFormState.email = feedbackForm.email.value;
+  feedbackFormState.message = feedbackForm.message.value;
+
+  console.log(`Feedback Form Submit: ${JSON.stringify(feedbackFormState)}`);
+
+  localStorage.removeItem(LS_FEEDBACK_FORM_STATE_KEY);
+  feedbackForm.reset();
+}
